fc4032ec5141f0c913b91e522073e965235cbf86
szantilas87 <szantilas@gmail.com>
2020-01-06 07:37:57 +0000

    Navbar & OrganizationState Provieder

---
+++ client/src/context/organization/OrganizationState.js
@@ -0,0 +1,63 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import organizationReducer from './organizationReducer';
+import OrganizationContext from './organizationContext';
+import {
+  ADD_ORGANIZATION,
+  DELETE_ORGANIZATION,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_ORGANIZATION,
+  FILTER_ORGANIZATIONS,
+  CLEAR_FILTER
+} from '../types';
+
+const OrganizationState = props => {
+  const initialState = {
+    organizations: [
+      {
+        id: 1,
+        name: 'org1',
+        rate: '10'
+      },
+      {
+        id: 2,
+        name: 'org2',
+        rate: '20'
+      },
+      {
+        id: 3,
+        name: 'org3',
+        rate: '30'
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(organizationReducer, initialState);
+
+  // Add Organization
+
+  // Delete Organization
+
+  // Set Current Organization
+
+  // Clear Current Organization
+
+  // Update Organization
+
+  // Filter Organizations
+
+  // Clear Filter
+
+  return (
+    <OrganizationContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}{' '}
+    </OrganizationContext.Provider>
+  );
+};
+
+export default OrganizationState;
